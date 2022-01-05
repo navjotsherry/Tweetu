d9799b924684818345a06b25636778ccfa508e0f
unknown <navjotsherryy@gmail.com>
2022-01-05 06:12:34 +0530

    Initial Commit

---
+++ Tweetu/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tweetu project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tweetu.settings')
+
+application = get_wsgi_application()
